23d1eb2522febbe3ef66d1f765e5c958dfb9e844
Robert Hull <robertjhull@protonmail.com>
2021-06-21 20:04:33 -0700

    Add project files.

---
+++ Bookstore.Core/BookServices.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Bookstore.Core
+{
+    public class BookServices : IBookServices
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookServices(IDbClient dbClient)
+        {
+            _books = dbClient.GetBooksCollection();
+        }
+        public List<Book> GetBooks()
+        {
+            return _books.Find(book => true).ToList();
+        }
+
+        public Book AddBook(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public Book GetBook(string id)
+        {
+            return _books.Find(book => book.Id == id).First();
+        }
+
+        public void DeleteBook(string id)
+        {
+            _books.DeleteOne(book => book.Id == id);
+        }
+
+        public Book UpdateBook(Book book)
+        {
+            GetBook(book.Id);
+            _books.ReplaceOne(b => b.Id == book.Id, book);
+            return book;
+        }
+    }
+}
---
+++ Bookstore.Core/IBookServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Core
+{
+    public interface IBookServices
+    {
+        List<Book> GetBooks();
+        Book GetBook(string id);
+        Book AddBook(Book book);
+        void DeleteBook(string id);
+        Book UpdateBook(Book book);
+    }
+}
